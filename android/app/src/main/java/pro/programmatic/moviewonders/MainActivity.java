d4d8b52e5ffc2c24d0f1d1425c2f83b8b40c39ad
Johannes Raggam <thetetet@gmail.com>
2021-02-01 00:08:51 +0100

    add youtubeplayer plugin

--- android/app/src/main/java/pro/programmatic/moviewonders/MainActivity.java
+++ android/app/src/main/java/pro/programmatic/moviewonders/MainActivity.java
@@ -2,6 +2,7 @@ package pro.programmatic.moviewonders;
 
 import android.os.Bundle;
 
+import com.abpjap.plugin.youtubeplayer.YoutubePlayer;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
@@ -15,7 +16,7 @@ public class MainActivity extends BridgeActivity {
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
+      add(YoutubePlayer.class);
     }});
   }
 }
